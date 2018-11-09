dfa11610d9c94ab78e3c4703b0765c80d5aa47e2
Quinten Clement <quintenclement@gmail.com>
2018-11-09 11:06:22 -0600

    moved project to new repo

---
+++ CustomListmk2/MyList.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListmk2
+{
+    public class MyList<T> : IEnumerable
+    {
+        public T[] items;
+        public T[] tempArray;
+
+        int count;
+        int cap;
+        public int Count { get { return count; } }
+        public MyList()
+        {
+            cap = 4;
+            count = 0;
+            items = new T[cap];
+
+        }
+        public void Add(T item)
+        {
+            CheckIfCapped();
+            items[count] = item;
+            count++;
+        }
+        public void CheckIfCapped()
+        {
+            if (count == cap - 1)
+            {
+                cap += 4;
+                StoreData();
+                CreateNewArray();
+                TransferData();
+            }
+        }
+        public void StoreData()
+        {
+            tempArray = new T[count];
+            for (int i = 0; i < count; i++)
+            {
+                tempArray[i] = items[i];
+            }
+        }
+        public void TransferData()
+        {
+            for (int i = 0; i < tempArray.Count(); i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+        }
+        public void CreateNewArray()
+        {
+            items = new T[cap];
+        }
+        public void Remove(int index)
+        {
+            while (count - 1 > index)
+            {
+                items[index] = items[index + 1];
+                index++;
+            }
+            count--;
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return items[index];
+            }
+            set
+            {
+                items[index] = value;
+            }
+        }
+        public static MyList<T> operator +(MyList<T> list1, MyList<T> list2)
+        {
+            MyList<T> list = new MyList<T>();
+            for (int i = 0; i < list1.count; i++)
+            {
+                list.Add(list1[i]);
+            }
+            for (int j = 0; j < list2.count; j++)
+            {
+                list.Add(list2[j]);
+            }
+            return list;
+        }
+        public static MyList<T> operator -(MyList<T> list1, MyList<T> list2)
+        {
+            for (int i = 0; i < list1.count; i++)
+            {
+                for (int j = 0; j < list2.count; j++)
+                {
+                    if (list1[i].Equals(list2[j]))
+                    {
+                        list1.Remove(i);
+                    }
+                }
+            }
+            return list1;
+        }
+        public override string ToString()
+        {
+            string NewString = "";
+            for (int i = 0; i < count; i++)
+            {
+                items[i].ToString();
+                NewString += items[i];
+            }
+            return NewString;
+        }
+        public MyList<T> Zip(MyList<T> list1)
+        {
+            MyList<T> list = new MyList<T>();
+            for (int i = 0; i < count && i < list1.count; i++)
+            {
+                list.Add(this[i]);
+                list.Add(list1[i]);
+            }
+            return list;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return this[i];
+            }  
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ CustomListmk2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListmk2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list1 = new MyList<int>();
+            MyList<int> list2 = new MyList<int>();
+            int expected = 10;
+            list1.Add(1);
+            list1.Add(3);
+            list1.Add(5);
+            list2.Add(2);
+            list2.Add(4);
+            list2.Add(6);
+            list2.Add(8);
+            list1.Zip(list2);
+        }
+    }
+}
